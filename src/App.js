f6f5795d3da1e656a11cf771d89a6246f2df6275
ajaygajulaeverest <ajay.gajula@everest.engineering>
2023-12-15 13:06:09 +0530

    Add images to storage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./css/App.css";
-import App2 from "./components/application.js";
+import ReactPlayer from "react-player";
+import vedio from "./assests/samplevedio.mp4";
+import Upload from "./components/application.js";
 
 
 function App() {
+ 
   return (
     <div>
+      <div>
+        <h1>Sample vedio to create account</h1>
+        <ReactPlayer
+          controls={true}
+          url={vedio}
+          height="300px"
+          width="750px"
+        ></ReactPlayer>
+      </div>
+
       <div class="user">
         <h1>Sign-in User</h1>
       </div>
@@ -29,8 +42,85 @@ function App() {
           />
         </div>
       </div>
-      <App2 />
+      <Upload />
     </div>
   );
 }
 export default App;
+
+// const config = {
+//   bucketName: "simpleaj",
+//   dirName: "media" /* optional */,
+//   region: "Asia Pacific (Mumbai) ap-south-1",
+//   accessKeyId: "AKIAYZ4ROK5J7QZUH747",
+//   secretAccessKey: "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 ",
+// };
+
+
+
+// function App() {
+//   // Create state to store file
+//   const [file, setFile] = useState(null);
+
+//   // Function to upload file to s3
+//   const uploadFile = async () => {
+//     // S3 Bucket Name
+//     const S3_BUCKET = "simpleaj";
+
+//     // S3 Region
+//     const REGION = "ap-south-1";
+
+//     // S3 Credentials
+//     AWS.config.update({
+//       accessKeyId: "AKIAYZ4ROK5JSPTC4DGR",
+//       secretAccessKey: "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",
+//     });
+//     const s3 = new AWS.S3({
+//       params: { Bucket: S3_BUCKET },
+//       region: REGION,
+//     });
+
+//     // Files Parameters
+
+//     const params = {
+//       Bucket: S3_BUCKET,
+//       Key: file.name,
+//       Body: file,
+//     };
+
+//     // Uploading file to s3
+
+//     var upload = s3
+//       .putObject(params)
+//       .on("httpUploadProgress", (evt) => {
+//         // File uploading progress
+//         console.log(
+//           "Uploading " + parseInt((evt.loaded * 100) / evt.total) + "%"
+//         );
+//       })
+//       .promise();
+
+//     await upload.then((err, data) => {
+//       console.log(err);
+//       // Fille successfully uploaded
+//       alert("File uploaded successfully.");
+//     });
+//   };
+//   // Function to handle file and store it to file state
+//   const handleFileChange = (e) => {
+//     // Uploaded file
+//     const file = e.target.files[0];
+//     // Changing file state
+//     setFile(file);
+//   };
+//   return (
+//     <div className="App">
+//       <div>
+//         <input type="file" onChange={handleFileChange} />
+//         <button onClick={uploadFile}>Upload</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/components/application.js
+++ src/components/application.js
@@ -1,17 +1,94 @@
 // import { Welcome } from "./componenet";
-export default function App2() {
+
+import { useState } from "react";
+import AWS from "aws-sdk";
+
+
+const validFileTypes = ["image/jpg", "image/jpeg", "image/png"];
+// const URL = "/images";
+export default function Upload() {
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState('');
+
+  // Function to upload file to s3
+  const uploadFile = async () => {
+    // S3 Bucket Name
+    const S3_BUCKET = "simpleaj";
+
+    // S3 Region
+    const REGION = "ap-south-1";
+
+    // S3 Credentials
+    AWS.config.update({
+      accessKeyId: "AKIAYZ4ROK5JSPTC4DGR",
+      secretAccessKey: "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",
+    });
+    const s3 = new AWS.S3({
+      params: { Bucket: S3_BUCKET },
+      region: REGION,
+    });
+
+    // Files Parameters
+
+    const params = {
+      Bucket: S3_BUCKET,
+      Key: file.name,
+      Body: file,
+    };
+
+    // Uploading file to s3
+
+    var upload = s3
+      .putObject(params)
+      .on("httpUploadProgress", (evt) => {
+        // File uploading progress
+        console.log(
+          "Uploading " + parseInt((evt.loaded * 100) / evt.total) + "%"
+        );
+      })
+      .promise();
+
+    await upload.then((err, data) => {
+      console.log(err);
+      // Fille successfully uploaded
+      alert("File uploaded successfully.");
+    });
+  };
+  // Function to handle file and store it to file state
+  const handleFileChange = (e) => {
+    // Uploaded file
+    const file = e.target.files[0];
+    if (!validFileTypes.find((type) => type === file.type)) {
+      setError("File must be in JPEG/PNG format");
+      return;
+    }
+    // Changing file state
+    setFile(file);
+  };
+
+  // 
+
   return (
     <div>
-      <div  class="name2">
+      <div class="name2">
         <div>
-          <h3>File Upload using React!</h3>
+          <h3>Please upload your profile picture</h3>
           <div>
-            <input type="file" />
-            <button>Upload!</button>
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={uploadFile}>Upload!</button>
+
+            {/* <Text fontsize="lg" color="red.300">
+              {error}{" "}
+            </Text> */}
           </div>
+          {error && (
+            <label fontsize="lg" color="red.300">
+              {error}
+            </label>
+          )}
         </div>
       </div>
-      <div >
+      <div>
         <button type="reset" class="cancel">
           Cancel
         </button>
